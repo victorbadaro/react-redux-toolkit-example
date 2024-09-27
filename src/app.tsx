73ad10bb4fa89dfc64000f61f832b1ce219b9d7d
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-27 15:11:38 -0300

    feat: add a header component

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,19 @@
+import { Header } from './components/header';
+
 export function App() {
-	return <h1>Hello, Redux Toolkit!</h1>;
+	return (
+		<div className="space-y-8 min-h-screen bg-zinc-900 text-zinc-100">
+			<Header />
+
+			<main className="container mx-auto">
+				<div className="flex flex-col gap-4">
+					<h1 className="text-2xl font-semibold">Users</h1>
+
+					<div className="flex items-center justify-center h-72 border border-dashed border-zinc-700 rounded-lg">
+						<p>Users list is empty.</p>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
