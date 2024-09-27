ecbc138e26c20023e554f975b1dcacdc468e0688
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-27 15:19:43 -0300

    feat: add a list component to show created users

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import { Header } from './components/header';
+import { UsersList } from './components/users-list';
 
 export function App() {
 	return (
@@ -9,9 +10,13 @@ export function App() {
 				<div className="flex flex-col gap-4">
 					<h1 className="text-2xl font-semibold">Users</h1>
 
-					<div className="flex items-center justify-center h-72 border border-dashed border-zinc-700 rounded-lg">
-						<p>Users list is empty.</p>
-					</div>
+					{Array.from({ length: 3 }).length > 0 ? (
+						<UsersList />
+					) : (
+						<div className="flex items-center justify-center h-72 border border-dashed border-zinc-700 rounded-lg">
+							<p>Users list is empty.</p>
+						</div>
+					)}
 				</div>
 			</main>
 		</div>
